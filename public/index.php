1161ba58daa3d92494ebe93b470575f7aa93b9c1
alftunner <alftunner@gmail.com>
2021-09-22 21:38:33 +0300

    2. Продолжаю писать маршрутизацию (гибкая с регулярками)

--- public/index.php
+++ public/index.php
@@ -4,22 +4,11 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
+require '../app/controllers/Main.php';
+require '../app/controllers/Posts.php';
 
-Router::add('posts/add', [
-    'controller' => 'Posts',
-    'action' => 'add'
-]);
-Router::add('posts/', [
-    'controller' => 'Posts',
-    'action' => 'index'
-]);
-Router::add('', [
-    'controller' => 'Main',
-    'action' => 'index'
-]);
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$'); // создаёт именованные ключи для совпадений с регулярками
 
-if (Router::matchRoute($query)) {
-    showMeArr(Router::getCurrentRoute());
-} else {
-    echo '404';
-}
+Router::dispatch($query);
+
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -1,29 +1,83 @@
 <?php
 
+/**
+ * класс для построения маршрутов
+ */
 class Router
 {
-    protected static $routes = []; // Массив со всеми маршрутами
-    protected static $currentRoute = []; // Текущий маршрут, который отрабатывает в данный момент
 
+    /**
+     * таблица маршрутов
+     * @var array
+     */
+    protected static $routes = [];
+
+    /**
+     * Текущий маршрут, который отрабатывает в данный момент
+     * @var array
+     */
+    protected static $currentRoute = [];
+
+    /**
+     * Добавляет маршрут в таблицу маршрутов
+     * @param $regexp - регулярное выражение маршрута
+     * @param array $route - маршрут [controller, action, param]
+     */
     public static function add($regexp, $route = []) {
         self::$routes[$regexp] = $route;
     }
 
+    /**
+     * Возвращает таблицу маршрутов
+     * @return array
+     */
     public static function getRoutes() {
         return self::$routes;
     }
 
+    /**
+     * Возвращает текущий маршрут
+     * @return array
+     */
     public static function getCurrentRoute() {
         return self::$currentRoute;
     }
 
+    /**
+     * ищет URL в таблице маршрутов
+     * @param string $url входящий URL
+     * @return bool
+     */
     public static function matchRoute($url) {
         foreach (self::$routes as $pattern => $route) {
-            if ($url == $pattern) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                foreach ($matches as $key => $value) {
+                    if (is_string($key)) {
+                        $route[$key] = $value;
+                    }
+                }
+                if(!isset($route['action'])) {
+                    $route['action'] = 'index';
+                }
                 self::$currentRoute = $route;
+                showMeArr(self::$currentRoute);
                 return true;
             }
         }
         return false;
+    }
+
+    public static function dispatch($url) {
+        if (self::matchRoute($url)) {
+            $controller = self::$currentRoute['controller'];
+            if(class_exists($controller)) {
+                echo 'OK';
+            } else {
+                echo "Контроллер <b>$controller</b> не найден";
+            }
+        } else {
+            http_response_code(404); //Код ошибки выводит в консоль
+            include '404.html';
+        }
     }
 }
